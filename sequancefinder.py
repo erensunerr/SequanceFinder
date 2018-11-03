b57b562055c506a135ad2c1c934f7cd850861ec0
gerroo <gerensuner@gmail.com>
2018-11-03 12:19:49 -0800

    Added Doctests

--- sequancefinder.py
+++ sequancefinder.py
@@ -1,23 +1,34 @@
 #!/usr/bin/python3
-while True:
-	a = float(input("1.Number = "))
-	b = float(input("2.Number = "))
-	c = int(input("How many numbers in between : "))
-	d = int(input("By Adding or Subtracting 1 | By Division or Multiplication 0\n"))
-	if d == 1 :
-		z =((b-a)/c)
+def main():
+    while True:
+    	a = float(input("1.Number = "))
+    	b = float(input("2.Number = "))
+    	c = int(input("How many numbers in between : "))
+    	d = int(input("By Adding or Subtracting 1 | By Division or Multiplication 0\n"))
+        #TODO: add harmonic series
+        print('=',findSeq(a,b,c,d))
+
+def findSeq(num1,num2, numInBet, method):
+    """
+    Function to find sequences.
+    >>>findSeq(5,25,3,1):
+    5
+    >>>findSeq(3,81,2,0)
+    9
+    """
+    if method == 1 : # Arithmetic
+		z =((num2-num1)/numInBet)
 		p = 0
 		while p < 10:
-			print(a) 
-			a +=z
+			print(num1)
+			num1 += z
 			p += 1
-		print("Adds = "+str(z))
-	elif d == 0:
-		z = (b/a)**(1/(c+1))
+	elif method == 0:#Geometric
+		z = (num2/num1)**(1/(numInBet+1))
 		p = 0
 		while p < 10:
-			print (a)
-			a = a*z
+			print (num1)
+			num1 = num1*z
 			p += 1
-		print("Multiply by = "+str(z))
-#Elif
+
+    return z
